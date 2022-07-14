c258ea40fc862a47a48ff4a3e8b55b342a3e60f6
Alireza <devtool95@gmail.com>
2022-07-14 16:34:53 +0430

    refactoring : refactoring file and folders

--- src/core/index.ts
+++
@@ -1,1 +0,0 @@
-export {default as GlobalStyled} from "./globalStyle"
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./core/localization/i18n.js";
+import "./localization/i18n.js";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
