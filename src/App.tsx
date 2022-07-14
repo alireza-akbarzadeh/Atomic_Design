2152c08d57398e31c8f48c2dbd2bc86132082b89
Alireza <devtool95@gmail.com>
2022-07-14 10:15:10 +0430

    add head to App AFile

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,7 @@ const App = () => {
   const { t } = useTranslation();
   return (
     <div id={"alireza"} className="Alireza">
-      <span id={"mmad"}>
-        <ul>
-          <li>
-            <a href="">Alireza Akbrazadeh bejandi</a>
-          </li>
-          <li>
-            <a href="">Saeed Blue green black</a>
-          </li>
-        </ul>
-      </span>
-      {t("Good Bay")}
+      <h3>Alireza</h3>
     </div>
   );
 };
