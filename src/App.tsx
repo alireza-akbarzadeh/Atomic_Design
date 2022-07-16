6e1aef5a429bc40e610439a90ff88f79ce7d3c72
Alireza <devtool95@gmail.com>
2022-07-16 16:28:34 +0430

    change App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { useTranslation } from "react-i18next";
 
 const App = () => {
-  const { t } = useTranslation();
-  return (
-    <div id={"alireza"} className="Alireza">
-      <h3>Alireza</h3>
-      <h3>WTF</h3>
-      <h3>Im Fucked up</h3>
-      <h3>This is Not Fir</h3>
-    </div>
-  );
+    return (
+        <h3>Learn React test</h3>
+    );
 };
 
 export default App;
